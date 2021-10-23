f71d6a32047786e2f004fdeccf5b0c7cd8fa655c
이승호 <seungho@iseunghos-MacBook-Pro.local>
2021-10-23 16:27:41 +0900

    Learn status

---
+++ src/App2.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class App2 extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
+}
+
+export default App2;
