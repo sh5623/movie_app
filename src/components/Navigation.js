0a35eef1f5b9927ecdb305d92a4532376d3e11f6
xxxx-jeung <bioman3238@gmail.com>
2021-11-10 09:39:21 +0900

    로그아웃 기능 구현

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,29 @@
 import React from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 function Navigation() {
+  const optionAxios = {
+    headers: {
+      "content-Type": "application/json",
+    },
+  };
+  function DoLogout(props) {
+    axios
+      .post("http://localhost:8080/user/logout", optionAxios)
+      .then((res) => {})
+      .catch((error) => {});
+  }
+
   return (
     <div className="nav">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/tour">Tour</Link>
+      <Link to="/" onClick={DoLogout}>
+        Logout
+      </Link>
     </div>
   );
 }
